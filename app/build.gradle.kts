4a7d47a5a31a30f7d1a949db67f971091635a329
Takuya Chinen <e245745@ie.u-ryukyu.ac.jp>
2025-01-08 16:34:39 +0900

    refix

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ plugins {
 }
 
 java {                                      
-    sourceCompatibility = JavaVersion.VERSION_17
+    sourceCompatibility = JavaVersion.VERSION_21
 }
 
 repositories {
